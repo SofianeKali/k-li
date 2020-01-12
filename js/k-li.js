2d93756202de125d1dd35bd98982858690974a6b
SofianeKali <kalizahir@yahoo.fr>
2020-01-12 21:44:38 +0100

    Add files via upload

---
+++ js/k-li.js
@@ -0,0 +1,7 @@
+$(window).scroll(function() {
+    if ($(window).scrollTop() >= 100) {
+        $('nav').addClass(' fixed-header');
+    } else {
+        $('nav').removeClass('fixed-header');
+    }
+});
